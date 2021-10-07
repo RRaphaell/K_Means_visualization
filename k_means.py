09e8af0f84e25a17ddc00acd2dd3d7c1a959d578
RRaphaell <raffo.kalandadze@gmail.com>
2021-10-08 01:49:16 +0400

    add little refactoring

--- k_means.py
+++ k_means.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 
-def kmeans(features, k, num_iters=100):
-    """ Use kmeans algorithm to group features into k clusters.
-
+def kmeans(features, k, num_iterations=100):
+    """
     This function makes use of numpy functions and broadcasting to speed up the
     first part(cluster assignment) of kmeans algorithm.
 
-    Hints
-    - You may find np.repeat and np.argmin useful
+    K-Means algorithm can be broken down into following steps:
+        1. Randomly initialize cluster centers
+        2. Assign each point to the closest center
+        3. Compute new center of each cluster
+        4. Stop if cluster assignments did not change
+        5. Go to step 2
 
     Args:
         features - Array of N features vectors. Each row represents a feature
@@ -26,8 +29,8 @@ def kmeans(features, k, num_iters=100):
     assert N >= k, 'Number of clusters cannot be greater than number of points'
 
     # Randomly initalize cluster centers
-    idxs = np.random.choice(N, size=k, replace=False)
-    centers = features[idxs]
+    center_indexes = np.random.choice(N, size=k, replace=False)
+    centers = features[center_indexes]
     assignments = np.zeros(N, dtype=np.uint32)
 
     # Compute new center of each cluster
@@ -36,7 +39,7 @@ def kmeans(features, k, num_iters=100):
             center_points = features[assignments == i]
             centers[i] = np.mean(center_points, axis=0)
 
-    for n in range(num_iters):
+    for n in range(num_iterations):
         features_broadcast = np.tile(features, (k, 1))
         centers_broadcast = np.repeat(centers, N, axis=0)
 
